ca84b6de1355466625ad10d69ae7cada98f27af5
mvking5464 <mvking5464@gmail.com>
2024-04-17 22:32:35 +0530

    register page completed

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -18,17 +18,17 @@ const UserSchema = new Schema(
   },
   { timestamps: true }
 );
-// --------NOT WORKING (SO ADD BYCRYP IN BACKEND AND VALIDATE THROUGH ZOD)
-// Do this after validation
-UserSchema.post("validate", function (s) {
-  console.log(this.password);
-  console.log(s.password);
-  s.password = "OKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK";
-  console.log("OKKKKKKKKKKKKK");
-  console.log({ arguments });
-  console.log("sjdfn");
-  // The 'arguments' object cannot be referenced in an arrow function in ES3 and ES5. Consider using a standard function expression.
-});
+// // --------NOT WORKING (SO ADD BYCRYP IN BACKEND AND VALIDATE THROUGH ZOD)
+// // Do this after validation
+// UserSchema.post("validate", function (s) {
+//   console.log(this.password);
+//   console.log(s.password);
+//   s.password = "OKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK";
+//   console.log("OKKKKKKKKKKKKK");
+//   console.log({ arguments });
+//   console.log("sjdfn");
+//   // The 'arguments' object cannot be referenced in an arrow function in ES3 and ES5. Consider using a standard function expression.
+// });
 console.log("OKKKKKKKKKKKKK");
 
 export const User = models?.User || model("User", UserSchema);
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,41 +1,79 @@
 "use client"; // Change this by transfering all the client component to different file
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function RegisterPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [creatingUser, setCreatingUser] = useState(false);
+  const [userCreated, setUserCreated] = useState(false);
+  const [error, setError] = useState(false);
+
   //   Optimise here as no need to fetch from the same backend (harkirat class)
-  function handleFormSubmit(e: any) {
+  async function handleFormSubmit(e: any) {
     e.preventDefault();
-    console.log(1);
-    fetch("/api/register", {
-      method: "POST",
-      body: JSON.stringify({ email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
-    console.log(2);
+    setCreatingUser(true);
+
+    try {
+      const response = await fetch("/api/register", {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+        headers: { "Content-Type": "application/json" },
+      });
+      console.log(response);
+      response.ok ? setUserCreated(true) : setError(true);
+      setCreatingUser(false);
+    } catch (e) {
+      console.log({ Errrrrrrrrrrrrr: e });
+      setCreatingUser(false);
+    }
   }
 
   return (
     <section className="mt-8">
       <h1 className=" text-center text-primary text-4xl mb-4">Register</h1>
+      {userCreated && (
+        <div className="my-4 text-center">
+          User created Successfully.{" "}
+          <Link className=" underline" href={"/login"}>
+            Login Now &raquo;
+          </Link>
+        </div>
+      )}
+      {error && (
+        <div className="my-4 text-center text-red-700">
+          Error creating user.{" "}
+        </div>
+      )}
       <form className=" block max-w-xs mx-auto " onSubmit={handleFormSubmit}>
         <input
+          disabled={creatingUser}
           type="email"
           placeholder="email"
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => {
+            setEmail(e.target.value);
+            setUserCreated(false);
+            setError(false);
+          }}
         />
         {JSON.stringify(email)}
         <input
+          disabled={creatingUser}
           type="password"
           placeholder="password"
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={(e) => {
+            setPassword(e.target.value);
+            setUserCreated(false);
+            setError(false);
+          }}
         />
         {JSON.stringify(password)}
-        <button type="submit">Register</button>
+        <button type="submit" disabled={creatingUser}>
+          Register
+        </button>
         <div className=" my-4 text-center text-gray-500">
           or login with provider
         </div>
@@ -43,6 +81,12 @@ export default function RegisterPage() {
           <Image src={"/image.png"} alt={"google"} height={24} width={24} />
           Login with google
         </button>
+        <div className=" text-center my-4 text-gray-500 border-t pt-4">
+          Already have an account?{" "}
+          <Link className=" underline" href={"/href"}>
+            Login here &raquo;
+          </Link>
+        </div>
       </form>
     </section>
   );
