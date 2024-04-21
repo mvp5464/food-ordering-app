7c778cf1ea0fd57248016482fbca5859b6bec91e
mvking5464 <mvking5464@gmail.com>
2024-04-21 23:09:52 +0530

    Checking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
             <Header />
             {children}
             <footer className=" border-t p-8 text-center text-gray-500 mt-16">
-              &copy; 2024 All rights reserved
+              &copy; 2024 All rights reserved.
             </footer>
           </AppProvider>
         </main>
