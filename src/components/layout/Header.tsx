a2838fc54fb7edb13bfb6a7988ac4676c33883d5
mvking5464 <mvking5464@gmail.com>
2024-04-22 23:40:34 +0530

    added login/logout toggle

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,11 +2,12 @@
 
 import React from "react";
 import Link from "next/link";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 export default function Header() {
   const session = useSession();
   console.log(session);
+  const status = session.status;
   return (
     <header className=" flex justify-between items-center">
       <nav className="flex gap-8 font-semibold text-gray-500 items-center">
@@ -19,13 +20,25 @@ export default function Header() {
         <Link href={""}>Contact</Link>
       </nav>
       <nav className=" flex gap-4 items-center text-gray-500 font-semibold">
-        <Link href={"/login"}>Login</Link>
-        <Link
-          href={"/register"}
-          className=" bg-primary rounded-full px-8 py-2 text-white"
-        >
-          Register
-        </Link>
+        {status == "authenticated" && (
+          <button
+            onClick={() => signOut()}
+            className=" bg-primary rounded-full px-8 py-2 text-white"
+          >
+            Logout
+          </button>
+        )}
+        {status == "unauthenticated" && (
+          <>
+            <Link href={"/login"}>Login</Link>
+            <Link
+              href={"/register"}
+              className=" bg-primary rounded-full px-8 py-2 text-white"
+            >
+              Register
+            </Link>
+          </>
+        )}
       </nav>
     </header>
   );
